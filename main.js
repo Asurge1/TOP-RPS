ddc697708529818e722b5cf82d7bb31395493a8a
Asurge <106493802+Asurge1@users.noreply.github.com>
2024-04-28 19:51:37 +1000

    Can play game using buttons now, functional*. Missing locking on win or lose & missing design

--- main.js
+++ main.js
@@ -1,66 +1,64 @@
-/*
-Rock	Paper Scissors
-0			1			2
-
-Comp choice
-	Pick 0, 1, 2
-Player choice
-	Request input
-	Convert input to 0, 1, 2
-
-Compare choices
-	If === tie
-	P - C	=	
-	0	-	1	=	-1	Lose	
-	0	-	2	=	-2	Win		
-	1	-	0	=	1		Win		
-	1	-	2	=	-1	Lose	
-	2	-	0	=	2		Lose	
-	2	-	1	=	1		Win		
-
-	-1 or 2 is a loss
-	-2 or 1 is a win
-Return victor
-
-*/
 let compChoice = 0;
 let playerChoice = 0;
 let points = 0;
 let match = 0;
+let winningPoints = 5;
+
+const buttons = document.querySelectorAll("button");
+const score = document.querySelector(".score");
+const round = document.querySelector(".round");
+const result = document.querySelector(".result");
+const verbose = document.querySelector(".verbose");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+		colorSwap(button);
+		playRound(button.textContent);
+  });
+});
+
+function colorSwap(e)
+{
+	let R = Math.random() * 255;
+	let G = Math.random() * 255;
+	let B = Math.random() * 255;
+	let str = "rgb(" + R + " " + G + " " + B + ")";
+	e.style.background = str;
+}
+
 function getComputerChoice() {
 	return Math.floor(Math.random() * 3);
 }
-
-
-
 function winLoseLogic(match) {
 	if (match === -1 || match === 2) {
-		console.log("You lost this round");
+		round.textContent = "You lost this round";
 		points--;
 	}
 	if (match === 1 || match === -2) {
-		console.log("You won this round");
+		round.textContent = "You won this round";
 		points++;
 	}
 	if (match === 0) {
-		console.log("This match was a draw")
+		round.textContent = "This match was a draw";
 	}
 	return points;
 }
 function ChoiceToNum(choice) {
 	switch (choice) {
-		case 'r':
+		case 'Rock':
 			choice = 0;
 			break;
-		case 'p':
+		case 'Paper':
 			choice = 1;
 			break;
-		case 's':
+		case 'Scissors':
 			choice = 2;
 			break;
 		default:
 			console.warn("Invalid choice: You must choose between Rock, Paper and Scissors (Or any word starting with R, P, S)");
-			playGame();
+			// playGame();
 			return;
 	}
 	return choice;
@@ -80,35 +78,46 @@ function verboseChoices(choice) {
 			console.error("Invalid input in verboseChoices, this should be impossible unless manually calling")
 	}
 }
-function playRound()
+function playRound(choice)
 {
 	compChoice = getComputerChoice();
-	playerChoice = prompt("Please enter your hand for Rock, Paper, Scissors: ").toLowerCase()[0];
+	playerChoice = ChoiceToNum(choice);
 
-	playerChoice = ChoiceToNum(playerChoice);
-	let choices = "The Player chose: " + verboseChoices(playerChoice) + " & the Computer chose: " + verboseChoices(compChoice);
-	console.log(choices);
-
-	gameResult = winLoseLogic(playerChoice - compChoice);
-
-	return gameResult;
+	verbose.textContent = "The Player chose: " + verboseChoices(playerChoice) + " & the Computer chose: " + verboseChoices(compChoice);
+	match++;
+	winLoseLogic(playerChoice - compChoice)
+	victoryLoss(points);
 }
 function playGame() {
-
+	let res = '';
 	for (let index = 0; index < 5; index++) {
 		points += playRound();
 	}
-	if(points > 0)
-	{
-		console.log("Congrats, you won Rock, Paper, Scissors!");
+	if (points > 0) {
+		res="Congrats, you won Rock, Paper, Scissors!";
 	}
-	if(points < 0)
-	{
-		console.log("Sorry, you lose.");
+	if (points < 0) {
+		res="Sorry, you lose.";
 	}
-	if(points === 0)
+	if (points === 0) {
+		res="Sorry, this match ended in a draw.";
+	}
+	result.textContent = res;
+}
+function victoryLoss(result)
+{
+
+	score.textContent = "Current Score: " + points +  " Round: " + match;
+	if(result === winningPoints)
 	{
-		console.log("Sorry, this match ended in a draw.");
+		score.textContent = "The Player wins with "  + result +  " points on round " + match;
+		points = 0;
+		match = 0;
+	}
+	if(result === -winningPoints)
+	{
+		score.textContent = "The Computer wins with "  + Math.abs(result) +  " points on round " + match;
+		points = 0;
+		match = 0;
 	}
 }
-
